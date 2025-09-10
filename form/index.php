b73bb11ee76acea38122cc751b7477b01e7247b6
Anatoliy Staroselskiy <Tolyan25y@yandex.ru>
2025-09-10 14:13:19 +0100

    add border animation

---
+++ form/index.php
@@ -0,0 +1,11 @@
+<!-- форма обратной связи -->
+
+<form action="" method="">
+    <input type="text" name="login">
+    <input type="password" name="pass">
+    <input type="email" name="email">
+    <input type="tel" name="tel">
+
+    <input type="submit" value="Отправить данные">
+
+</form>
